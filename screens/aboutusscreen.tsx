e681cdf20e3750fe64d274811b5fd743c32d2217
Williams <widorenyin0@gmail.com>
2025-06-22 20:46:52 +0100

    Add about us module components

---
+++ screens/aboutusscreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@/components/Restyle";
+
+const ServiceScreen = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">About Us Screen</Text>
+    </Box>
+  );
+};
+
+export default ServiceScreen;
